ee307a4eca8f1a91f7e88396c170ca5d65781173
Abooo0d <abdsadalden2001@gmail.com>
2024-08-15 11:04:00 +0300

    Abood

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import AuthLayout from "./_Auth/AuthLayout";
 import Form from "./_Auth/Forms/Form";
+import Form from "./";
 function App() {
   return (
     <Routes>
