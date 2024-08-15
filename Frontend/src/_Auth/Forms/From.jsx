8583d5c7c9a674aa60ede490d3e93007088b266d
Abooo0d <abdsadalden2001@gmail.com>
2024-08-15 11:02:07 +0300

    Adding the Main packages  And Start Creating The Router

---
+++ Frontend/src/_Auth/Forms/From.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form action="#" method="POST" className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="johndoe@example.com"
+                required
+                autoComplete="email"
+                className=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 outline-none duration-300"
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Form;
